7be6b682d973df85127ac08d83a4a5c8ff285010
mjainamshah <jainam.shah@student.moringaschool.com>
2023-10-24 15:43:28 +0300

    COMPLETED

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return (  
+<nav>
+  <a key={NavBar.id} href="#home"> Home</a>
+  <a key={NavBar.id} href="#about"> About</a>
+  <a key={NavBar.id} href="#projects"> Projects</a>
+</nav>
+)}
 
 export default NavBar;
